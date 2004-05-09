8fdfc5fc8c309ed0d68ed1c8312eea5d7efb0074
twoface <twoface@merlin.mongueurs.net>
2004-05-09 19:00:02 +0000

    [project @ 964] Maybe this is updated, maybe not...

--- example.pl
+++ example.pl
@@ -4,12 +4,10 @@ use strict;
 die("MPD.pm not found!\n") unless -f "MPD.pm";
 require("MPD.pm");
 
-#my $x = MPD->new('10.0.0.37',2100); # Do this for specifing server and/or port.
+#my $x = MPD->new('localhost',2100); # Do this for specifing server and/or port.
 my $x = MPD->new();
 $x->connect();
-
 print "Example: Shows MPD.pm used to write mpc-like output\n\n";
-
 print($x->gettitle,"\n");
 print("[".$x->{state}."] #".$x->{song}."/".$x->{playlistlength}."   ".$x->gettimeformat."\n");
 print("volume: ".$x->{volume}."%  repeat: ".$x->{repeat}."   random: ".$x->{random}."\n");
@@ -17,7 +15,7 @@ print("volume: ".$x->{volume}."%  repeat: ".$x->{repeat}."   random: ".$x->{rand
 print "\n\nExample: Shows list of all files, directories and playlists in a specific directory.\n";
 print "NB! This is to show how listallinfo() / lsinfo() is used with nextinfo(). It should only be used this way!\n\n";
 
-$x->listallinfo('Classical');
+$x->listallinfo('Alternative');
 my %foo;
 while(%foo = $x->nextinfo)
 {
