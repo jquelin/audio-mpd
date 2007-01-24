b6cb4d6ed28fbdd36241598fa5f141217f7cf3b5
jq <jq@merlin.mongueurs.net>
2007-01-24 17:36:35 +0000

    [project @ 5280] updates backported from Audio::MPD 0.12.3

--- scripts/example.pl
+++ scripts/example.pl
@@ -1,18 +1,17 @@
 #!/usr/bin/perl -w
 use strict;
 use Data::Dumper;
+use Audio::MPD;
 
 ### NOTE! ###
 # THIS SCRIPT DOES NOT CONTAIN ERRORHANDLING, AND MAY THEREFORE FAIL ON
 # CERTAIN ACTIONS
 ############
 
-die("MPD.pm not found!\n") unless -f "MPD.pm";
-require("MPD.pm");
-
 #my $x = MPD->new('localhost',2100); # Do this for specifing server and/or port.
-my $x = MPD->new();
-print "Example: Shows MPD.pm used to write mpc-like output\n\n";
+my $x = Audio::MPD->new('localhost', 6601);
+
+print "Example: Shows Audio::MPD used to write mpc-like output\n\n";
 print($x->get_title,"\n");
 print("[".$x->{state}."] #".($x->{song} || 'n/a')."/".$x->{playlistlength}."   ".$x->get_time_format."\n");
 print("volume: ".$x->{volume}."%  repeat: ".$x->{repeat}."   random: ".$x->{random}."\n");
