65afe72e8c8d3e8dfa6ef689ccf964463c3c2e1a
jq <jq@merlin.mongueurs.net>
2007-03-17 14:52:05 +0000

    [project @ 5636] added Rev keyword to trunk

--- lib/Audio/MPD/Collection.pm
+++ lib/Audio/MPD/Collection.pm
@@ -27,7 +27,7 @@ use base qw[ Class::Accessor::Fast ];
 __PACKAGE__->mk_accessors( qw[ _mpd ] );
 
 
-our ($VERSION) = '$Rev$' =~ /(\d+)/;
+our ($VERSION) = '$Rev: 5636 $' =~ /(\d+)/;
 
 #--
 # Constructor
