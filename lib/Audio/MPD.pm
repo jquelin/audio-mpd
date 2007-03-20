b76abe5e624c5bc88d4a703b2b08670798d9c588
jq <jq@merlin.mongueurs.net>
2007-03-20 16:25:49 +0000

    [project @ 5662] fixed bug #25553

--- lib/Audio/MPD.pm
+++ lib/Audio/MPD.pm
@@ -30,7 +30,7 @@ use base qw[ Class::Accessor::Fast ];
 __PACKAGE__->mk_accessors( qw[ _host _password _port collection version ] );
 
 
-our $VERSION = '0.15.1';
+our $VERSION = '0.15.2';
 
 
 #--
@@ -451,7 +451,7 @@ sub fade {
 #
 sub play {
     my ($self, $number) = @_;
-    $number ||= '';
+    $number = '' unless defined $number;
     $self->_send_command("play $number\n");
 }
 
