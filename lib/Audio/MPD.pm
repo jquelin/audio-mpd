492b589eef2c72135804c9c9c79af7269c7cdb6d
Jérôme Quelin <jquelin@gmail.com>
2009-10-16 18:48:59 +0200

    removed pod that will be added by dzil

--- lib/Audio/MPD.pm
+++ lib/Audio/MPD.pm
@@ -876,21 +876,3 @@ L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Audio-MPD>
 
 =back
 
-
-
-=head1 AUTHOR
-
-Jerome Quelin, C<< <jquelin@cpan.org> >>
-
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright (c) 2005 Tue Abrahamsen, all rights reserved.
-Copyright (c) 2006 Nicolas J. Humfrey, all rights reserved.
-Copyright (c) 2007-2009 Jerome Quelin, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
